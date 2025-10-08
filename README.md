a8e8795e57b4083230e3ec50a53c31c0eda85700
mh-shihan <mahmodul.no.1@gmail.com>
2025-10-08 16:17:41 +0600

    feat: integrate Tailwind CSS and Redux Toolkit, refactor App component
    - Added Tailwind CSS for styling and updated index.css to import Tailwind.
    - Integrated Redux Toolkit and React-Redux for state management.
    - Refactored App component to remove old styles and implement a new layout with increment and decrement buttons.
    - Removed unused App.css file to streamline styles.
    - Updated vite.config.ts to include Tailwind CSS plugin.

--- README.md
+++ README.md
@@ -3,14 +3,32 @@
 ## Project Installation
 
 ```bash
-npm create vite
+npm create vite@latest
 project name: my-react-app
 select framework: React
 select variant: TypeScript
 ```
 
+### Install Tailwind CSS
+
+```bash
+npm install tailwindcss @tailwindcss/vite
+tailwindcss() # this plugin in vite.config.ts
+@import "tailwindcss"   # in index.css
+```
+
+### Navigate to the project directory and start the development server
+
+```bash
 cd my-react-app
 npm run dev
+```
+
+Install Redux Toolkit and React-Redux
+
+```bash
+npm install @reduxjs/toolkit react-redux
+```
 
 ## React Compiler
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="min-h-screen w-full flex items-center justify-center bg-gray-50">
+      <div className="flex items-center gap-6 border-3 border-purple-300 rounded-md p-10 bg-slate-50 shadow-lg">
+        <button className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white cursor-pointer">
+          Increment
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+        <h1 className="text-6xl m-10">0</h1>
+
+        <button className="px-3 py-2 rounded-md bg-red-500 text-xl font-semibold text-white cursor-pointer">
+          Decrement
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
